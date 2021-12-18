48257ce4c173333b0976919423eb5ea570d98e6b
CodeKrakken <newholm1@hotmail.com>
2021-12-18 15:24:02 +0000

    Restore errant code to ListTemplate

--- public/app.js
+++ public/app.js
@@ -1,42 +1,10 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
 import { ListTemplate } from './classes/ListTemplate.js';
-let docOne;
-let docTwo;
-docOne = new Invoice('yoshi', 'web work', 250);
-docTwo = new Payment('Mario', 'plumbing work', 200);
-let docs = [];
-docs.push(docOne);
-docs.push(docTwo);
-const me = {
-    name: 'CodeKrakken',
-    age: 25,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log('I spent ' + amount);
-        return amount;
-    }
-};
-const greetPerson = (person) => {
-    console.log('Hello, ' + person.name);
-};
-console.log(me);
-greetPerson(me);
-const inv1 = new Invoice('Mario', 'Saving the Princess', 330);
-const inv2 = new Invoice('Luigi', 'Hangin about, wearing green', 330);
-let invoices = [];
-invoices.push(inv1);
-invoices.push(inv2);
-invoices.forEach(invoice => {
-    console.log(invoice.format());
-});
-console.log(invoices);
 const form = document.querySelector('.new-item-form');
 console.log(form.children);
 const type = document.querySelector('#type');
-const tofrom = document.querySelector('#toFrom');
+const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 const ul = document.querySelector('ul');
--- src/app.ts
+++ src/app.ts
@@ -3,64 +3,13 @@ import {Payment}      from './classes/Payment.js'
 import {HasFormatter} from './interfaces/HasFormatter.js'
 import {ListTemplate} from './classes/ListTemplate.js'
 
-let docOne: HasFormatter;
-let docTwo: HasFormatter;
-
-docOne = new Invoice('yoshi', 'web work', 250);
-docTwo = new Payment('Mario', 'plumbing work', 200);
-
-let docs: HasFormatter[] = [];
-
-docs.push(docOne)
-docs.push(docTwo)
-
-interface IsPerson {
-  name: string;
-  age: number;
-  speak(a: string): void;
-  spend(a: number): number;
-}
-
-const me: IsPerson = {
-  name: 'CodeKrakken',
-  age: 25,
-  speak(text: string): void {
-    console.log(text);
-  },
-  spend(amount: number): number {
-    console.log('I spent ' + amount)
-    return amount;
-  }
-}
-
-const greetPerson = (person: IsPerson) => {
-  console.log('Hello, ' + person.name)
-}
-
-console.log(me)
-greetPerson(me)
-
-const inv1 = new Invoice('Mario', 'Saving the Princess', 330)
-const inv2 = new Invoice('Luigi', 'Hangin about, wearing green', 330)
-
-let invoices: Invoice[] = [];
-invoices.push(inv1)
-invoices.push(inv2)
-
-invoices.forEach(invoice => {
-  console.log(invoice.format())
-})
-
-console.log(invoices)
-
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-
 console.log(form.children)
 
-const type    = document.querySelector('#type'    ) as HTMLSelectElement;
-const tofrom  = document.querySelector('#toFrom'  ) as HTMLSelectElement;
-const details = document.querySelector('#details' ) as HTMLSelectElement;
-const amount  = document.querySelector('#amount'  ) as HTMLSelectElement;
+const type    = document.querySelector('#type'    ) as HTMLInputElement;
+const tofrom  = document.querySelector('#tofrom'  ) as HTMLInputElement;
+const details = document.querySelector('#details' ) as HTMLInputElement;
+const amount  = document.querySelector('#amount'  ) as HTMLInputElement;
 
 const ul = document.querySelector('ul')!;
 const list = new ListTemplate(ul);
